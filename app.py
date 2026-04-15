ee81c971d6106777f8b1c03a39fd8debaf8fd975
Dan Everett <dan.everett@INNERGY.COM>
2026-04-15 08:51:37 -0600

    hide menu and footer

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import streamlit as st
 
+hide_menu = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_menu, unsafe_allow_html=True)
+
 st.title("Innergy Tool")
 st.write("Hello, world!")
